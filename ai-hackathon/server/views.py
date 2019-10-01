65ef441158c0a8941fb86f89ec22babfe637eeb6
simar4895 <luthrasimarpreet@gmail.com>
2019-10-01 20:35:10 +1000

    first commit

--- ai-hackathon/server/views.py
+++ ai-hackathon/server/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+import json
+from .src import responder
+
+# class chatbot(APIView):
+#   def post(self, request):
+#     model = load_model('./api/titanic_model.pk')
+#     data = request.data
+#     prediction = classify_passenger(model = model, data = data)
+#     return(Response(prediction))
