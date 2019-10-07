aa360f123e549c5822d1b3b9db98af94189f24cf
kartikeya-dewal <kartikeya.dewal@gmail.com>
2019-10-07 20:42:58 +1100

    added axios to make api calls to server

--- ai-hackathon/server/api.py
+++ ai-hackathon/server/api.py
@@ -1,9 +1,22 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from server.models import User
 from rest_framework import viewsets, permissions
 from .serializers import UserSerializer
+from .functions import botResponse
 
 # User Viewset
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response({"something": "my custom JSON"})
+
+    def update(self, request, *args, **kwargs):
+        print(request.data)
+        res = botResponse(request.data['userText'])
+        return Response({"message": res})
+
