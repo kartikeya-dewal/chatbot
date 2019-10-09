24577c8de343d92acb7db7220263ecaac8f59208
g-brodiei <chunjuichen@gmail.com>
2019-10-09 23:17:17 +1100

    add Candidate list

--- ai-hackathon/client/src/components/page/CandidateList.js
+++ ai-hackathon/client/src/components/page/CandidateList.js
@@ -1,51 +1,59 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
-import Candidate from './Cadidate'
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
+
 const CandidateList = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchUsers;
+  }, [fetchUsers]);
+  const fetchUsers = async () => {
+    users = await axios.get(`/api/user/`);
+  };
+
   const userList = [
     {
       id: 1,
-      name: 'John Doe',
+      name: "John Doe",
       overallScore: 100
     },
     {
       id: 2,
-      name: 'Mary Jane',
+      name: "Mary Jane",
       overallScore: 79
     },
     {
       id: 3,
-      name: 'Kat Heart',
+      name: "Kat Heart",
       overallScore: 72
     },
     {
       id: 4,
-      name: 'Patrick Hendrick',
+      name: "Patrick Hendrick",
       overallScore: 45
     },
     {
       id: 5,
-      name: 'Ben McLoren',
+      name: "Ben McLoren",
       overallScore: 62
     }
-  ]
+  ];
 
   return (
     <Fragment>
       <div className="display1">User List</div>
       <ul>
-        {
-          userList.map((user, index) => {
-            return (
-              <li key={index}>
-                Score: {user.overallScore} <Link to={"recruiter/user/" + user.id}>{user.name}</Link>
-              </li>
-            )
-          })
-        }
+        {userList.map((user, index) => {
+          return (
+            <li key={index}>
+              Score: {user.overallScore}{" "}
+              <Link to={`recruiter/user/${user.id}`}>{user.name}</Link>
+            </li>
+          );
+        })}
       </ul>
     </Fragment>
-  )
-}
+  );
+};
 
-export default CandidateList
+export default CandidateList;
