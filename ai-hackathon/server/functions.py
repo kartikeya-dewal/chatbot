de2a354f0295540f154173e5401da3f05632d169
simar4895 <luthrasimarpreet@gmail.com>
2019-10-09 15:23:20 +1100

    created partial chat

--- ai-hackathon/server/functions.py
+++ ai-hackathon/server/functions.py
@@ -1,4 +1,39 @@
+from .src import responder
+import pandas as pd
+
+### Chat variables ###
+
+
+chat_corpus = {
+    "greeting" : "Could you please tell me about your educational background?",
+    "education" : "hey",
+    "reason" : "yo"
+}
+
+index = 0
+
+chat_log = pd.DataFrame()
+
+greeting_responses = ["HI", "HEY", "HELLO", "GREETINGS", "GREETING", "SUP", "WHATS UP","HOWDY","YO"]
+
+
+### Chatbot response ###
+
 def botResponse(text):
-    result = "awesome"
-    return result
+    global index
+    chat_index = list(chat_corpus.keys())[index]
+    # Response to greeting message
+    if chat_index == "greeting":
+        text_grams = responder.word_grams(text)
+        for gram_index in range(0,len(text_grams)):
+            if text_grams[gram_index] in greeting_responses:
+                reply = chat_corpus.get(chat_index)
+                index = index + 1
+                chat_log.loc[index,"index"] = index
+                chat_log.loc[index,"response"] = text
+            else:
+                reply = "Sorry, please reply with a proper greeting response"
+    else:
+        reply = chat_corpus.get(chat_index)
+    return reply
 
