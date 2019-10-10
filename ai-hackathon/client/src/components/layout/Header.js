ebd3410f3f33f6a08af414c3f7be8d711022d7d9
g-brodiei <chunjuichen@gmail.com>
2019-10-10 12:48:00 +1100

    add link on header

--- ai-hackathon/client/src/components/layout/Header.js
+++ ai-hackathon/client/src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <nav className='navbar navbar-light bg-light'>
-      <span className='navbar-brand mb-0 h1'>Chatbot</span>
+      <span className='navbar-brand mb-0 h1'><Link to="/">Chatbot</Link></span>
     </nav>
   );
 };
