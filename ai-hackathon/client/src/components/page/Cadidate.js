782a807ddec1034fe349bcf019215298b9001303
g-brodiei <chunjuichen@gmail.com>
2019-10-10 17:45:29 +1100

    add candidate name and score

--- ai-hackathon/client/src/components/page/Cadidate.js
+++ ai-hackathon/client/src/components/page/Cadidate.js
@@ -4,31 +4,33 @@ import SentiChart from "../chart-widget/sentiChart";
 import SkillChart from "../chart-widget/skillChart";
 import ResChart from "../chart-widget/resChart";
 import { useParams } from "react-router-dom";
-// import axios from 'axios';
+import axios from 'axios';
 
 const Candidate = () => {
   const id = useParams();
-  // console.log(id);
+  console.log(id);
 
-  // const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
  
-  // useEffect(() => {
-  //   const result = axios
-  //     .get(`/api/user/` + id.id )
-  //     .then(result => {
-  //       setTimeout(() => {
-  //         // console.log(result.data.data);
-  //         setUser(result.data.data);
-  //       }, 1000);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    const result = axios
+      .get(`/api/user/` + id.id )
+      .then(result => {
+        setTimeout(() => {
+          console.log(result.data.data);
+          setUser(result.data.data);
+        }, 1000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
 
   return (
     <Fragment>
+      <h3 className="text-center">Candidate: {user.name}</h3>
+      <h4 className="text-center">Overall Score: {user.overall_score}</h4>
       <div className="row charts-wrapper">
         <div className="col-12 col-md-6">
           <EduChart id={id} />
