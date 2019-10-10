da00e91a086bfc4a54c7b248876860d8c59d6b7f
g-brodiei <chunjuichen@gmail.com>
2019-10-10 16:52:17 +1100

    RestAPI data to all chart follow url params

--- ai-hackathon/client/src/components/chart-widget/resChart.js
+++ ai-hackathon/client/src/components/chart-widget/resChart.js
@@ -1,14 +1,34 @@
-import React, { Fragment } from "react";
-import { ResponsiveContainer, PieChart, Pie, Cell, Sector} from "recharts";
+import React, { Fragment, useState, useEffect } from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell, Sector } from "recharts";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 const resChart = () => {
+  const id = useParams();
+  const [res, setRes] = useState(false);
+
+  useEffect(() => {
+    const result = axios
+      .get(`/api/user/` + id.id)
+      .then(result => {
+        setTimeout(() => {
+          console.log(result.data.data.research_value);
+          setRes(result.data.data.research_value);
+        }, 1000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   const data = [
     {
-      "name": "example1",
-      "value": 0.8
+      name: "example1",
+      value: 0.8
     },
     {
-      "name": "example2",
-      "value": 0.2
+      name: "example2",
+      value: 0.2
     }
   ];
 
@@ -18,19 +38,23 @@ const resChart = () => {
       <h2 className="text-center">Research Chart</h2>
       <ResponsiveContainer height={350} width="100%">
         <PieChart>
-        <Pie
-          data={data}
-          dataKey="value"
-          nameKey="name"
-          startAngle={-180}
-          endAngle={180}
-          label
-        >
-          {
-            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-          }
-        </Pie>
-      </PieChart>
+          <Pie
+            data={res}
+            dataKey="value"
+            nameKey="name"
+            startAngle={-180}
+            endAngle={180}
+            label
+            outerRadius={120}
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
       </ResponsiveContainer>
     </Fragment>
   );
--- ai-hackathon/client/src/components/chart-widget/sentiChart.js
+++ ai-hackathon/client/src/components/chart-widget/sentiChart.js
@@ -1,38 +1,66 @@
-import React, { Fragment } from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts'
+import React, { Fragment, useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const sentiChart = () => {
-  const data = [
-    {
-      "sentiValue": 0.87
-    },
-    {
-      "sentiValue": 0.8
-    },
-    {
-      "sentiValue": -0.2
-    },
-    {
-      "sentiValue": 0.38
-    },
-    {
-      "sentiValue": 0.45
-    }
-  ]
+  const id = useParams();
+  const [senti, setSenti] = useState(false);
+
+  useEffect(() => {
+    const result = axios
+      .get(`/api/user/` + id.id)
+      .then(result => {
+        setTimeout(() => {
+          console.log(result.data.data.sentimental_level);
+          setSenti(result.data.data.sentimental_level);
+        }, 1000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  // const data = [
+  //   {
+  //     sentiValue: 0.87
+  //   },
+  //   {
+  //     sentiValue: 0.8
+  //   },
+  //   {
+  //     sentiValue: -0.2
+  //   },
+  //   {
+  //     sentiValue: 0.38
+  //   },
+  //   {
+  //     sentiValue: 0.45
+  //   }
+  // ];
 
   return (
     <Fragment>
       <h2 className="text-center">Sentimental Level</h2>
       <ResponsiveContainer height={350} width="100%">
-        <LineChart data={data}>
+        <LineChart data={senti}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis hide />
           <YAxis domain={[-1, 1]} />
+          <Tooltip />
           <Line type="monotone" dataKey="sentiValue" />
         </LineChart>
       </ResponsiveContainer>
     </Fragment>
-  )
-}
+  );
+};
 
-export default sentiChart
+export default sentiChart;
